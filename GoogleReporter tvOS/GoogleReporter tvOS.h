c1d551799a4f5f02f3a9f7a2fdf84668e6b1d7f6
Kristian Andersen <hello@kristian.co>
2017-05-26 18:28:30 +0200

    Added tvOS target

---
+++ GoogleReporter tvOS/GoogleReporter tvOS.h
@@ -0,0 +1,19 @@
+//
+//  GoogleReporter tvOS.h
+//  GoogleReporter tvOS
+//
+//  Created by Kristian Andersen on 26/05/2017.
+//  Copyright © 2017 Kristian Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GoogleReporter tvOS.
+FOUNDATION_EXPORT double GoogleReporter_tvOSVersionNumber;
+
+//! Project version string for GoogleReporter tvOS.
+FOUNDATION_EXPORT const unsigned char GoogleReporter_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GoogleReporter_tvOS/PublicHeader.h>
+
+
